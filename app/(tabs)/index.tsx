d08b783535a2275a11a5134c485ced0dc9bab953
ferrad <ferrad.fm@gmail.com>
2024-01-25 22:26:22 +0100

    chore: wip

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,43 @@
-import { View, Text, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  FlatList,
+  ListRenderItem,
+  Image,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Feather } from "@expo/vector-icons";
 import { useTheme } from "@react-navigation/native";
+import AllActionCards from "../../components/AllActionCards";
 
 const index = () => {
   const { colors } = useTheme();
+
   return (
     <SafeAreaView className="">
       <View
         style={{
           paddingVertical: 15,
-          height: 150,
+          height: 160,
           backgroundColor: "#392F6B",
           position: "relative",
         }}
       >
-        <Text style={{ fontFamily: "GilroyRegular", color: "white" }}>
-          lorem100
-        </Text>
+        <View className="items-center justify-center">
+          <Image
+            className="h-5 w-36"
+            source={require("../../assets/images/pngegg.png")}
+          />
+        </View>
         <View
           style={{
             position: "absolute",
             borderRadius: 15,
             backgroundColor: "#F9F9FE",
 
-            top: 70,
+            top: 80,
             alignSelf: "center",
             width: "90%",
             marginHorizontal: 15,
@@ -51,14 +63,20 @@ const index = () => {
           <View className="w-20 h-20 bg-black rounded-full"></View>
         </View>
       </View>
-      <View className="p-5">
-        <View style={{ overflow: "hidden", borderRadius: 8 }}>
+      <View className="p-5 ">
+        <View
+          className="h-16 mt-10 mb-4"
+          style={{ overflow: "hidden", borderRadius: 8 }}
+        >
           <Pressable
-            className="flex-row h-16 mt-10 bg-[#392F6B]"
+            className="flex-row  bg-[#392F6B]"
             style={{ overflow: "hidden", borderRadius: 8 }}
-            android_ripple={{ color: "#3F405B" }}
+            android_ripple={{ color: "#3F405B", borderless: false }}
           >
-            <View className=" bg-[#635BFE] w-14 h-full p-3"></View>
+            <View
+              className=" bg-[#635BFE] w-14 h-full p-3"
+              style={{ borderRadius: 8 }}
+            ></View>
             <View className="flex-row items-center justify-between flex-1 h-full p-3 pr-5">
               <View className="space-y-1">
                 <Text
@@ -87,6 +105,7 @@ const index = () => {
             </View>
           </Pressable>
         </View>
+        <AllActionCards />
       </View>
     </SafeAreaView>
   );
---
+++ components/AllActionCards.tsx
@@ -0,0 +1,32 @@
+import { View, Text, ListRenderItem, Pressable, FlatList } from "react-native";
+import React from "react";
+
+const AllActionCards = () => {
+  const data = [1, 2, 3, 4, 5, 6];
+  const renderItem: ListRenderItem<any> = ({ item }) => {
+    return (
+      <View className="flex-1 h-20 overflow-hidden rounded-lg">
+        <Pressable
+          className="items-center justify-center w-full h-full space-y-5 border rounded-lg border-sky-200"
+          android_ripple={{ color: "#3F405B", borderless: false }}
+        >
+          <Text>{item}</Text>
+        </Pressable>
+      </View>
+    );
+  };
+  return (
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      numColumns={2}
+      columnWrapperStyle={{
+        flexDirection: "row",
+        gap: 10,
+        paddingVertical: 5,
+      }}
+    />
+  );
+};
+
+export default AllActionCards;
